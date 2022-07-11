d703152582036cf24601303a6245c15e7e9922d6
Jee Won Park <jtag5926@gmail.com>
2022-07-12 08:22:42 +1200

    Update PriorityQueue to use minHeap to improve performance

--- PriorityQueue.h
+++ PriorityQueue.h
@@ -1,48 +1,35 @@
 #pragma once
 #include <vector>
 #include <optional>
+#include <queue>
+
 
 template <typename T>
 class PriorityQueue
 {
 public:
-    std::optional<T> min() 
-    { 
-        if (len() == 0) 
-            return std::nullopt; 
-        FindMin();
-        return m_queue[m_minIndex.value()];
-    }
-    std::optional<T> pop()
+    std::optional<T> min()
     {
         if (len() == 0)
             return std::nullopt;
-        T ret = m_queue[m_minIndex.value()];
-        m_queue.erase(m_queue.begin() + m_minIndex.value());
-        m_minIndex = std::nullopt;
-        return ret;
+        return m_queue.top();
     }
-    void FindMin()
+    void pop()
     {
-        if (m_minIndex.has_value())
-            return;
-        auto min = m_queue[0];
-        m_minIndex = 0;
-        for (int i = 1; i < m_queue.size(); ++i)
-        {
-            auto val = m_queue[i];
-            if (val < min)
-            {
-                min = val;
-                m_minIndex = i;
-            }
-        }
+        m_queue.pop();
     }
-    size_t len() const noexcept { return m_queue.size(); }
-    void append(const T& item) { m_queue.emplace_back(item); m_minIndex = std::nullopt; }
-    
+
+    size_t len() const noexcept
+    {
+        return m_queue.size();
+    }
+
+    void append(const T& item)
+    {
+        m_queue.push(item);
+    }
+
 private:
-    std::vector<T> m_queue;
-    std::optional<int> m_minIndex{};
+    std::priority_queue<T, std::vector<T>, std::greater<T>> m_queue{};
 };
 
